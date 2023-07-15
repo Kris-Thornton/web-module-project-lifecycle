bee1ee8be77e48f041104104f3035741a0db7069
Kris Thornton <Kristhornton@ymail.com>
2023-07-15 07:04:41 -0700

    added cleanup

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -32,13 +32,36 @@ handleChange = evt => {
 }
 
 
+resetForm = () => this.setState({ ...this.state, todoNameInput: ''})
+setAxiosResponseError = (err) => this.setState({...this.state, error: err.response.data.message})
+
+
+
+
+postNewTodo = () => {
+  axios.post(URL, {name: this.state.todoNameInput})
+  .then(res => {
+    this.fetchAllTodos
+    this.resetForm()
+  })
+  .catch(this.setAxiosResponseError)
+}
+
+
+onTodoFormSubmit = evt => {
+  // bug here...... without prevent Default, item added to list just fine on DOM.  With preventDefault item only added to state and then to list on DOM when page is refreshed.
+  evt.preventDefault()
+  this.postNewTodo()
+}
+
+
 fetchAllTodos = () => {
   axios.get(URL)
   .then(res => {
     this.setState({ ...this.state, todos: res.data.data})
   })
   .catch(err => {
-    this.setState({...this.state, error: err.response.data.message})
+    this.setState(this.setAxiosResponseError)
   })
 }
 
@@ -62,7 +85,7 @@ render() {
             })
           }
         </div>
-        <form id="todoForm">
+        <form onSubmit={this.onTodoFormSubmit} id="todoForm">
           <input value={this.state.todoNameInput} onChange={this.handleChange} type="text" placeholder="Type todo"></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
