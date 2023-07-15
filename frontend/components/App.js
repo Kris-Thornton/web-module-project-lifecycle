b87ddc64e3ac7b6cea5dc621d4d2196cf9f0c046
Kris Thornton <Kristhornton@ymail.com>
2023-07-14 20:21:44 -0700

    following along to understand Postman.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import Form from './Form'
+import axios from 'axios'
+
+
+
 
 const URL = 'http://localhost:9000/api/todos'
 
+
+
+
+
+
 export default class App extends React.Component {
-  render() {
-    return null
+
+constructor() {
+  super();
+  this.state = {
+    todos: [],
+    error: '',
+  };
+}
+
+fetchAllTodos = () => {
+  axios.get(URL)
+  .then(res => {
+    this.setState({ ...this.state, todos: res.data.data})
+  })
+  .catch(err => {
+    this.setState({...this.state, error: err.response.data.message})
+  })
+}
+componentDidMount() {
+  this.fetchAllTodos();
+}
+
+  
+
+render() {
+    return (
+      <div>
+        <div id="error">Error: {this.state.error}</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
 export default class Form extends React.Component {
-  render() {
-    return null
-  }
+
+constructor() {
+  super();
+  this.state = {
+    input:''
+  };
 }
+
+handleChange = (e) => {
+  this.setState({
+    ...this.state,
+    input: e.target.value
+  })
+}
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  this.props.handleAdd(this.state.input)
+  console.log('submitted')
+}
+
+
+  render() {
+   return(
+    <>
+    <input onChange={this.handleChange}/>
+    <button onClick={this.handleSubmit} >Add to List</button>
+    </>
+   )
+  }
+ }
